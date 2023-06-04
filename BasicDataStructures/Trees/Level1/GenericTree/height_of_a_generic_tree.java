c0e7b9d1152c9e382c1634a6fe1fe10adde532ee
Anjali Rana <anjalirana7870@gmail.com>
2023-06-04 13:33:49 +0530

    addded file

--- BasicDataStructures/Trees/Level1/GenericTree/height_of_a_generic_tree.java
+++ BasicDataStructures/Trees/Level1/GenericTree/height_of_a_generic_tree.java
@@ -19,32 +19,35 @@ public class height_of_a_generic_tree{
     }
     public static int height(Node node){
         // handles base case i.e. only single node is geven : hieght of tree will be 0
-        int maxh = -1;  //in terms of edges
-        // int h = 0   // i terms of nodes
-        for(Node child: node.children){
-            int ch = height(child);
-            maxh = Math.max(maxh, ch);
+        int h = -1;
+        for(Node child : node.children){
+            int ch  = height(child);
+            h = Math.max(ch,h);
         }
-        maxh += 1;
-        return maxh;
+        h += 1;
+
+        return h;
     }
     public static void main(String[] args) {
-        int[] arr = {10, 20, -1, 30, 50, -1, 60, -1, -1, 40, -1, -1};
+        int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1};
         Node root = null;
         Stack<Node> st = new Stack<>();
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == -1){
                 st.pop();
             }
-            Node t = new Node();
-            t.data = arr[i];
-            if(st.size() > 0){
-                st.peek().children.add(t);
-            }
             else{
-                root = t;
+                Node t = new Node();
+                t.data = arr[i];
+                if(st.size() > 0){
+                    st.peek().children.add(t);
+                }
+                else{
+                    root = t;
+                }
+                st.push(t);
             }
-            st.push(t);
+           
         }
         display(root);
         int h = height(root);
